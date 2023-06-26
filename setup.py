c5da582883125bd299f8b45224ba9e1cc210d3c4
Xavier Medrano <xaviermedrano@Xaviers-MacBook-Pro.local>
2023-06-26 14:57:15 -0400

    update readme to refer to mailchimp

--- setup.py
+++ setup.py
@@ -8,8 +8,7 @@ with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    # TODO: change to mailchimp
-    name='django-salsa-auth',
+    name='django-mailchimp-auth',
     version='0.1',
     packages=find_packages(),
     include_package_data=True,
